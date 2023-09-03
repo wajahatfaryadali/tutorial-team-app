7be7d248a84cc40dbcb7fa2a824ccd8f49333cae
Wajahat Ali <wajahatfaryadali@gmail.com>
2023-09-03 17:53:45 +0500

    completed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,38 +4,28 @@ import { route } from '../data/routeConstants'
 
 const Nav = ({ }) => {
 
+    const location = useLocation();
     const [navButtons, setNavButtons] = useState([
         {
             name: "Home",
             url: route.baseUrl,
-            isCurrent: false,
         },
         {
             name: "Teams",
             url: route.teams,
-            isCurrent: false,
         }
     ])
 
-    const setActiveButton = (name) => {
-        const updatedList = navButtons.map(btn => ({
-            ...btn,
-            isCurrent: btn.name === name 
-        }))
-
-        setNavButtons(updatedList)
-    }
-    
 
     return (
         <div className='py-3 bg-[#548999] px-4 text-center fixed min-w-full shadow-xl shadow-white text-white'>
             <div className='flex gap-6 font-semibold text-xl'>
                 {navButtons?.map((item, i) =>
-                    <Link to={item.url} key={i} onClick={() => setActiveButton(item.name)} className={`hover:scale-105 duration-50 border-b-2 ${item.isCurrent === true ? "border-[#00fff7] " : "border-b-white"}`}>{item.name}</Link>
+                    <Link to={item.url} key={i} className={`hover:scale-105 duration-50`}>{item.name}</Link>
                 )}
             </div>
 
-        </div>
+        </div >
     )
 }
 
